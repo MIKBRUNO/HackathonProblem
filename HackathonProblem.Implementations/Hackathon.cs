419f0489d3c4e53b9de41f2a2941e8e71700a858
m.dyachenko2 <m.dyachenko2@g.nsu.ru>
2024-10-08 08:23:38 +0000

    moved hackathon implementation to implementation assembly

---
+++ HackathonProblem.Implementations/Hackathon.cs
@@ -0,0 +1,16 @@
+using HackathonProblem;
+namespace HackathonProblem.Implementations;
+
+public class Hackathon(IWishlistGenerator generator) : IHackathon
+{
+    private readonly IWishlistGenerator generator = generator;
+
+    public HackathonResult Perform(IEnumerable<IEmployee> teamleads, IEnumerable<IEmployee> juniors, IHRManager manager, IHRDirector director)
+    {
+        var teamleadsWishlists = generator.GenerateWishlists(teamleads, juniors);
+        var juniorsWishlists = generator.GenerateWishlists(juniors, teamleads);
+        var teams = manager.BuildTeams(teamleadsWishlists, juniorsWishlists);
+        double rate = director.CalculateSatisfaction(teams, teamleadsWishlists, juniorsWishlists);
+        return new HackathonResult(teams, rate);
+    }
+}
