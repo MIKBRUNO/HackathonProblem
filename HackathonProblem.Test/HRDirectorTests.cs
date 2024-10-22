3c39de87086cb1c4a48c5c455d4d564719e91920
m.dyachenko2 <m.dyachenko2@g.nsu.ru>
2024-10-22 08:15:45 +0000

    Выделил static class with test employees

--- HackathonProblem.Test/HRDirectorTests.cs
+++ HackathonProblem.Test/HRDirectorTests.cs
@@ -47,41 +47,17 @@ public class HRDirectorTests
         Assert.Equal(3, actual, 12);
     }
 
-    private static readonly IList<IEmployee> teamleads = [
-        new Employee(1, "UnoT"),
-        new Employee(2, "DosT"),
-        new Employee(3, "TresT")
-    ];
-
-    private static readonly IList<IEmployee> juniors = [
-        new Employee(4, "UnoJ"),
-        new Employee(5, "DosJ"),
-        new Employee(6, "TresJ")
-    ];
-
-    private static readonly IEnumerable<IWishlist> teamleadsWishlists = [
-        new Wishlist(teamleads[0], [juniors[0], juniors[1], juniors[2]]),
-        new Wishlist(teamleads[1], [juniors[1], juniors[2], juniors[0]]),
-        new Wishlist(teamleads[2], [juniors[2], juniors[0], juniors[1]])
-    ];
-
-    private static readonly IEnumerable<IWishlist> juniorssWishlists = [
-        new Wishlist(juniors[0], [teamleads[0], teamleads[1], teamleads[2]]),
-        new Wishlist(juniors[1], [teamleads[1], teamleads[2], teamleads[0]]),
-        new Wishlist(juniors[2], [teamleads[2], teamleads[0], teamleads[1]])
-    ];
-
     [Fact]
     public void CalculateSatisfaction_WithPredeterminedRatings_Test1()
     {
         // best teams
         IEnumerable<ITeam> teams = [
-            new Team(teamleads[0], juniors[0]),
-            new Team(teamleads[1], juniors[1]),
-            new Team(teamleads[2], juniors[2])
+            new Team(TestEmployees.teamleads[0], TestEmployees.juniors[0]),
+            new Team(TestEmployees.teamleads[1], TestEmployees.juniors[1]),
+            new Team(TestEmployees.teamleads[2], TestEmployees.juniors[2])
         ];
         var director = new HRDirector();
-        double actual = director.CalculateSatisfaction(teams, teamleadsWishlists, juniorssWishlists);
+        double actual = director.CalculateSatisfaction(teams, TestEmployees.teamleadsWishlists, TestEmployees.juniorssWishlists);
         Assert.Equal(3, actual, 12);
     }
 
@@ -91,12 +67,12 @@ public class HRDirectorTests
         // second for teamleads, worst for juniors
         // [2]*3 + [1]*3
         IEnumerable<ITeam> teams = [
-            new Team(teamleads[0], juniors[1]),
-            new Team(teamleads[1], juniors[2]),
-            new Team(teamleads[2], juniors[0])
+            new Team(TestEmployees.teamleads[0], TestEmployees.juniors[1]),
+            new Team(TestEmployees.teamleads[1], TestEmployees.juniors[2]),
+            new Team(TestEmployees.teamleads[2], TestEmployees.juniors[0])
         ];
         var director = new HRDirector();
-        double actual = director.CalculateSatisfaction(teams, teamleadsWishlists, juniorssWishlists);
+        double actual = director.CalculateSatisfaction(teams, TestEmployees.teamleadsWishlists, TestEmployees.juniorssWishlists);
         Assert.Equal(4.0 / 3.0, actual, 12);
     }
 
@@ -105,12 +81,12 @@ public class HRDirectorTests
     {
         // 1, 2, 3, 3, 2, 1
         IEnumerable<ITeam> teams = [
-            new Team(teamleads[0], juniors[2]),
-            new Team(teamleads[1], juniors[1]),
-            new Team(teamleads[2], juniors[0])
+            new Team(TestEmployees.teamleads[0], TestEmployees.juniors[2]),
+            new Team(TestEmployees.teamleads[1], TestEmployees.juniors[1]),
+            new Team(TestEmployees.teamleads[2], TestEmployees.juniors[0])
         ];
         var director = new HRDirector();
-        double actual = director.CalculateSatisfaction(teams, teamleadsWishlists, juniorssWishlists);
+        double actual = director.CalculateSatisfaction(teams, TestEmployees.teamleadsWishlists, TestEmployees.juniorssWishlists);
         Assert.Equal(18.0 / 11.0, actual, 12);
     }
 }
