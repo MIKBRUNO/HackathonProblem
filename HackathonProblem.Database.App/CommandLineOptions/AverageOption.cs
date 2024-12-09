c65b4a14da745f2696758e8ddcf4dec106db14eb
m.dyachenko2 <m.dyachenko2@g.nsu.ru>
2024-12-09 23:35:49 +0000

    leftovers

---
+++ HackathonProblem.Database.App/CommandLineOptions/AverageOption.cs
@@ -0,0 +1,6 @@
+using CommandLine;
+
+namespace HackathonProblem.Database.App.CommandLineOptions;
+
+[Verb("average", HelpText = "Calculate average stisfaction rate for all hackathonsin the database.")]
+public class AverageOptions { }
